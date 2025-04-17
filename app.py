b9c440105f66ced2100c44bf94e7f7f5489c4eae
Gnfw <ggnsyp@gmail.com>
2025-04-17 14:09:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -315,5 +315,6 @@ def check_password():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
